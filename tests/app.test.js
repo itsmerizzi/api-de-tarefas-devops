015409d85b0a6940aafaa17458a9a5411fba0733
Augusto Rizzi <82126968+itsmerizzi@users.noreply.github.com>
2025-10-28 21:38:36 -0300

    refactor test file to use updated app module

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('../app');
 
 let server;
 
@@ -24,4 +24,3 @@ describe('API de Tarefas', () => {
     expect(response.statusCode).toBe(200);
   });
 });
-
